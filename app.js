571fff5ca3ad37623ca2cc4137eda86642ba2fba
Asya Aleksandrova <mail.aleksandrova@yandex.ru>
2022-12-18 00:01:46 +0300

    DB_14_17

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
+const validateNewUser = require('./middlewares/validations');
 const errorHandler = require('./middlewares/errorhandler');
 
 const { PORT = 3000 } = process.env;
@@ -20,15 +21,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(/https*:\/\/\w+/),
-    email: Joi.string().required().regex(/\w+@\w+\.\w+/),
-    password: Joi.string().required().min(8),
-  }),
-}), createUser);
+app.post('/signup', validateNewUser, createUser);
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().regex(/\w+@\w+\.\w+/),
--- middlewares/validations.js
+++ middlewares/validations.js
@@ -1,27 +1,13 @@
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 
-// const validateUser = {
-//   body: Joi.object().keys({
-//     name: Joi.string().min(2).max(30),
-//     about: Joi.string().min(2).max(30),
-//     avatar: Joi.string().regex(/https?\/\/[\S]{1, }/),
-//     email: Joi.string().required().regex(/[\S]{1, }@[\w]{1, }\.[\w]{1, 3}/),
-//     password: Joi.string().required().min(8),
-//   }),
-// };
+const validateNewUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/https*:\/\/\w+/),
+    email: Joi.string().required().regex(/\w+@\w+\.\w+/),
+    password: Joi.string().required().min(8),
+  }),
+});
 
-// const validateSomeData = celebrate({
-//   params: Joi.object().keys({
-//      //валидаторы параметров адреса
-//      // в нашем случае это только id
-//   }),
-
-//   body: Joi.object().keys({
-//     //валидаторы данных передаваемых в body
-//   }),
-
-//   headers: Joi.object().keys({
-//     //валидатор заголовка, требуется проверять только
-//     //наличие заголовка authorization, в запросов которые требуют авторизации
-//   }).unknown(),
-// });
+module.exports = validateNewUser;
