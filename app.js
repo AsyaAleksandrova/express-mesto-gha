ed84beb9d180d06f5656b41370577d79d0a5ef62
Asya Aleksandrova <mail.aleksandrova@yandex.ru>
2022-12-17 20:13:41 +0300

    DB_14_7

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
-// const router = require('express').Router();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 // const { celebrate, Joi, errors } = require('celebrate');
 const { login, createUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
@@ -21,18 +19,9 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: '639a2f9b484ac8debf93d1ca',
-//   };
-//   next();
-// });
-
-app.use(cookieParser());
 app.post('/signup', createUser);
 app.post('/signin', login);
 
-// app.use(auth);
 app.use('/', auth, require('./routes/users'));
 app.use('/', auth, require('./routes/cards'));
 // app.use(errors());
--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
+const JWT_STRING = 'Nsl8TZXJzSYmL7TC5v5fRSaUPrUMas4TAap0NKqjqnaH3Q+0gSETQRRQz/3FYxW1zsQ8FC6QkwS2ZM4+auHH42C+dkbF1aA';
+
+const MONGO_DUPLICATE_ERROR_CODE = 11000;
 const OtherServerError = require('../errors/OtherServerError'); // 500 ERROR_CODE_OTHER
 const NotFoundError = require('../errors/NotFoundError'); // 404 ERROR_CODE_FIND
 const ValidationError = require('../errors/ValidationError'); // 400 ERROR_CODE_VALID
@@ -44,12 +48,16 @@ module.exports.createUser = (req, res, next) => {
       password: hash,
     }))
     .then((user) => {
-      res.status(200).send({ data: user });
+      res.status(201).send({
+        data: {
+          name: user.name, about: user.about, avatar: user.avatar, email: user.email, _id: user._id,
+        },
+      });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new ValidationError(`Переданые некорректные данные при создании пользователя: ${err.message}`));
-      } else if (err.code === 11000) {
+      } else if (err.code === MONGO_DUPLICATE_ERROR_CODE) {
         next(new ConflictError('Пользователь с таким email уже зарегистрирован'));
       } else {
         next(new OtherServerError(`Что-то пошло не так: ${err.message}`));
@@ -59,15 +67,18 @@ module.exports.createUser = (req, res, next) => {
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
+  if (!email || !password) {
+    next(new NotFoundError('Неправильные почта или пароль'));
+  }
   User
-    .findOne({ email })
+    .findOne({ email }).select('+password')
     .orFail(() => next(new NotFoundError('Неправильные почта или пароль')))
     .then((user) => {
       if (!bcrypt.compare(password, user.password)) {
         next(new AuthError('Неправильные почта или пароль'));
       } else {
-        const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-        res.status(200).cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+        const token = jwt.sign({ _id: user._id }, JWT_STRING, { expiresIn: '7d' });
+        res.status(200).send({ token });
       }
     })
     .catch((err) => {
